d94b6dfa6ab0715ad4006d606f28944c01543b20
Itai Stopel <itaistoppel@gmail.com>
2020-03-19 14:23:06 +0200

    Make react work with jupyter

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,56 @@
 import {
-  JupyterFrontEnd, JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from "@jupyterlab/application";
 
-import {
-  ICommandPalette, MainAreaWidget
-} from '@jupyterlab/apputils';
+import { ICommandPalette, MainAreaWidget } from "@jupyterlab/apputils";
 
-import {
-  Widget
-} from '@lumino/widgets';
+import { Widget } from "@lumino/widgets";
+import React from "react";
+import ReactDOM from 'react-dom';
 
 function initSearchWidget(content: Widget) {
-  let header = document.createElement('h2');
-  content.node.appendChild(header);
-  header.innerText = 'Search';
-  console.log('Added');
+  let searchHeader = <div>
+    <h2>Search Everything</h2>
+    <input/>
+  </div>;
+  ReactDOM.render(searchHeader, content.node);
 }
 
 /**
  * Initialization data for the deepsearch extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'deepsearch',
+  id: "deepsearch",
   autoStart: true,
   requires: [ICommandPalette],
   activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    console.log('JupyterLab extension deepsearch is activated!');
-    console.log('ICommandPalette 123:', palette);
+    console.log("JupyterLab extension deepsearch is activated!");
+    console.log("ICommandPalette 123:", palette);
     // Create a blank content widget inside of MainAreaWidget
     const content = new Widget();
-    const widget = new MainAreaWidget({content});
+    const widget = new MainAreaWidget({ content });
     initSearchWidget(content);
-    widget.id = 'deepsearch';
-    widget.title.label = 'Deepsearch';
+    widget.id = "deepsearch";
+    widget.title.label = "Deepsearch";
     widget.title.closable = true;
 
     // Add an application commend
-    const command: string = 'deepsearch:search';
+    const command: string = "deepsearch:search";
     app.commands.addCommand(command, {
-      label: 'Search Everywhere',
+      label: "Search Everywhere",
       execute: () => {
         if (!widget.isAttached) {
           // Attach the widget to the main work area if it's not there
-          app.shell.add(widget, 'main');
+          app.shell.add(widget, "main");
         }
         // Activate the widget
         app.shell.activateById(widget.id);
       }
     });
     // Add the command to the palette.
-    palette.addItem({command, category: 'Search'})
+    palette.addItem({ command, category: "Search" });
   }
 };
-
 
 export default extension;
